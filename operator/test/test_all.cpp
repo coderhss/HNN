c1dc7c4fd6cf913bd04dc41bd215843c93bd3d0b
coderhss <hss_seu@126.com>
2023-07-18 02:10:16 +0800

    feat: 添加test的cmake

--- operator/test/test_all.cpp
+++ operator/test/test_all.cpp
@@ -2,7 +2,10 @@
 // Created by bryab on 23-7-17.
 //
 #include <iostream>
+#include "gtest/gtest.h"
 int main(int argc, char* argv[]) {
+    testing::InitGoogleTest(&argc, argv);
+    auto ret = RUN_ALL_TESTS();
     std::cout << "hello world" << std::endl;
     return 0;
 }
